6aa6a5c0c9e6f8eaff1ee3d1bd4c5192c4af1ba0
Vincenzo Russo <v@artetecha.com>
2026-03-04 15:22:27 +0100

    Avada patch 423036

--- app/mypackagist/themes/Avada/functions.php
+++ app/mypackagist/themes/Avada/functions.php
@@ -19,11 +19,11 @@ if ( ! defined( 'AVADA_VERSION' ) ) {
 }
 
 if ( ! defined( 'AVADA_MIN_PHP_VER_REQUIRED' ) ) {
-	define( 'AVADA_MIN_PHP_VER_REQUIRED', '7.4' );
+	define( 'AVADA_MIN_PHP_VER_REQUIRED', '5.6' );
 }
 
 if ( ! defined( 'AVADA_MIN_WP_VER_REQUIRED' ) ) {
-	define( 'AVADA_MIN_WP_VER_REQUIRED', '5.7' );
+	define( 'AVADA_MIN_WP_VER_REQUIRED', '4.9' );
 }
 
 // Developer mode.
